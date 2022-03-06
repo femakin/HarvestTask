f6d2c99dab4e3b324106ae2766ed484459e2caaf
femi <oluwafemi.akinyemi@datamellon.com>
2022-03-06 05:13:34 +0100

    More changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,91 +1,79 @@
 import React from "react";
 import Layout from "../Layout/Layout";
-import './Home.css'
-import profileimage from '../Assets/Images/profileimage.svg'
-
-
+import "./Home.css";
+import profileimage from "../Assets/Images/profileimage.svg";
 
 export default function Home() {
+  const data = [
+    {
+      p: "Available balance",
+      price: "₦1,063,345.04",
+    },
+    {
+      p: "Impact Investments",
+      price: "₦1,007,345.04",
+    },
 
-const data = [
-  {
-    "p": "Available balance",
-    "price": "₦1,063,345.04",
-},
-{
-  "p": "Impact Investments",
-  "price": "₦1,007,345.04",
-},
+    {
+      p: "Total Savings",
+      price: "₦2,007,345.04",
+    },
 
-{
-  "p": "Total Savings",
-  "price": "₦2,007,345.04",
-},
-
-{
-  "p": "Total Returns",
-  "price": "₦56,000.00",
-},
-
-
-]
+    {
+      p: "Total Returns",
+      price: "₦56,000.00",
+    },
+  ];
 
   return (
     <div>
       <Layout>
         <div className="home-">
-          
-            <header>
-             
-              <div className="header-wrapper">
-                
+          <header>
+            <div className="header-wrapper">
               <div className="header-div">
-                <img src={profileimage} alt="profile-img" className="header-image" />  
+                <img
+                  src={profileimage}
+                  alt="profile-img"
+                  className="header-image"
+                />
               </div>
 
               <div className="header-content">
                 <p className="header-title">
-                  <span className="header-greetings">Hello, </span> 
+                  <span className="header-greetings">Hello, </span>
                   <p className="header-name">Oluwatobi</p>
-                </p>  
-          
-              </div>  
+                </p>
               </div>
+            </div>
+          </header>
 
-
-            </header>
-          
-
-
-
-
-      <div class="cover">
-       
-        <div className="cover-wrapper">
-          
-        {
-
-data.map((x, i)=>{
-  return (
-    <div  key={i} className="hero-wrapper" >
-
-      <p className="hero-text"  >{x.p}</p>
-      <p className="hero-price"  >{x.price}</p>
-
-    </div>
-  )
-})
-
-
-}
-
-          
-        </div>
-
-      </div>
-          
-          
+          <div class="cover-">
+          <div className="cover-wrapper--">
+              {data.map((x, i) => {
+                return (
+                  <div key={i} className="hero-wrapper-">
+                    <p className="hero-text">{x.p}</p>
+                    <p className="hero-price">{x.price}</p>
+                  </div>
+                );
+              })}
+            </div>  
           </div>
+
+
+          <section className="main-wrapper-text">
+            
+            <h1 className="main-watch">Watch your plans</h1>
+
+            <p className="watch-sub">
+            A goal without a plan is just a wish</p>
+            
+            
+        </section>
+
+         
+        </div>
       </Layout>
     </div>
   );
